e4fc9da8288793d10fc6408b5e04b11b0e0a21d9
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-09-14 09:19:35 +0700

    Time: 108 ms (46.49%), Space: 84.9 MB (77.85%) - LeetHub

---
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+
+        int maxValue = *std::max_element(nums.begin(), nums.end());
+
+        int maxLength = 0;
+        int currentLength = 0;
+
+        for (int num : nums){
+            if (num == maxValue){
+                currentLength++;
+                maxLength = max(maxLength, currentLength);
+            }
+            else currentLength = 0;
+        }   
+
+        return maxLength;
+
+    }
+};
