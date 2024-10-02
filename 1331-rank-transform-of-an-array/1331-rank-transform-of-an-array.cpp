82060b083627e0ea2484a2e3bb32fc2d5f527d8d
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-10-02 07:20:15 +0700

    Time: 71 ms (55.88%), Space: 45.5 MB (23.58%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        
+        vector <int> result (arr);
+        sort(result.begin(), result.end());
+
+        unordered_map <int, int> rank;
+
+        for (int& a : result) rank.emplace(a, rank.size()+1);
+
+        for (int i = 0; i < result.size(); ++i) result[i] = rank[arr[i]];
+
+        return result;
+
+    }
+};
