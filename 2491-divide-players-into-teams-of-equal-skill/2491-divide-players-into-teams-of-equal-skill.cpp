aed0554a462134b34c3003ca2c0dbef4ddc09ed1
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-10-04 17:45:36 +0700

    Time: 71 ms (23%), Space: 56.4 MB (56.4%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        long long n = skill.size();
+
+        sort(skill.begin(), skill.end());
+
+        long long totalSkill = skill[0] + skill[n-1];
+        long long chemistrySum = 0;
+
+        for (long long i = 0; i < n / 2; ++i) {
+            long long currentTeamSkill = skill[i] + skill[n - 1 - i];
+            
+            if (currentTeamSkill != totalSkill) {
+                return -1;
+            }
+
+            chemistrySum += skill[i] * skill[n - 1 - i];
+        }
+
+        return chemistrySum;
+    }
+};
