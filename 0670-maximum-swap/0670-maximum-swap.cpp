63253ba0968423343594029ed140eadb1899040b
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-10-17 12:07:12 +0700

    Time: 3 ms (38.18%), Space: 7.6 MB (31.77%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+
+        string str = to_string(num);
+        int n = str.size();
+
+        int index = -1;
+        int swap1, swap2 = -1;
+
+        for (int i = n-1; i >= 0; i--){
+
+            if (index == -1 || str[i] > str[index]) index = i;
+            else if (str[i] < str[index]){
+                swap1 = i;
+                swap2 = index;
+            }
+
+        }
+
+        if (swap1 != -1 && swap2 != -1) swap(str[swap1], str[swap2]);
+
+        return stoi(str);
+
+    }
+};
